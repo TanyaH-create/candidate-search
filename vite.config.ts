8e65b64f5f625c9860e1bed3cc293a3d2a22a918
Tanya <dougtanyah@gmail.com>
2025-01-26 18:36:09 -0500

    additions to vite.config.ts file required by render

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   envDir: './environment',
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Ensure the server is accessible on all network interfaces
+    port: parseInt(process.env.PORT) || 5173, // Use the PORT environment variable if available
+    allowedHosts: [
+      'candidate-search-challenge-tlh.onrender.com', // Add the host you want to allow
+    ],
+  }
 });
